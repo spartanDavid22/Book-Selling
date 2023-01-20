fe9a037e4380e294cb5408d70c33aaf6e8f58c71
spartanDavid22 <90160219+spartanDavid22@users.noreply.github.com>
2023-01-19 19:51:27 -0500

    Added an Edit button to allow to edit categories

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,7 +1,7 @@
 ﻿using BulkyBookWeb.Data;
 using BulkyBookWeb.Models;
 using Microsoft.AspNetCore.Mvc;
-//test
+//test 
 namespace BulkyBookWeb.Controllers
 {
     public class CategoryController : Controller
@@ -39,5 +39,42 @@ namespace BulkyBookWeb.Controllers
 
             return View(obj);
         }
+
+        //Handling Edits to Categories 
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryById = _db.Categories.Find(id);
+            
+            if (categoryById == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryById);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
     }
 }
--- Models/Category.cs
+++ Models/Category.cs
@@ -10,6 +10,7 @@ namespace BulkyBookWeb.Models
         [Required]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Range(1,100)]
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
