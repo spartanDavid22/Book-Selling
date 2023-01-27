87a845e9081b8dc78c422107efd808450a94b2b3
spartanDavid22 <90160219+spartanDavid22@users.noreply.github.com>
2023-01-27 11:03:16 -0500

    Added toastr for notifications and added TempData

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -35,6 +35,8 @@ namespace BulkyBookWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
 
@@ -71,6 +73,7 @@ namespace BulkyBookWeb.Controllers
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
 
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
 
@@ -111,6 +114,8 @@ namespace BulkyBookWeb.Controllers
 
             _db.Remove(obj);
             _db.SaveChanges();
+
+            TempData["success"] = "Category deleted successfully";
 
             return RedirectToAction("Index");
             
